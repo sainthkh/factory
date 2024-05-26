30560c99464f1fd26743afb5131ebc95266f863e
Kukhyeon Heo <sainthkh@gmail.com>
2024-05-26 21:01:17 +0900

    Draw image on click.

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,65 @@ use sdl2::render::{WindowCanvas, Texture};
 use sdl2::rect::{Point, Rect};
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, texture: &Texture) -> Result<(), String> {
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn new(x: i32, y: i32) -> Position {
+        Position { x, y }
+    }
+}
+
+enum EntityType {
+    Turret,
+    Conveyor,
+    Bullet,
+    Enemy,
+}
+
+struct Game {
+    positions: Vec<Position>,
+    entity_types: Vec<EntityType>,
+}
+
+impl Game {
+    fn new() -> Game {
+        Game {
+            positions: Vec::new(),
+            entity_types: Vec::new(),
+        }
+    }
+
+    fn add_entity(&mut self, position: Position, entity_type: EntityType) {
+        self.positions.push(position);
+        self.entity_types.push(entity_type);
+    }
+}
+
+fn render(canvas: &mut WindowCanvas, texture: &Texture, game: &Game) -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
 
-    let (width, height) = canvas.output_size()?;
-    let screen_position = Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, 32, 32);
+    // let (width, height) = canvas.output_size()?;
 
-    let sprite = Rect::new(0, 0, 32, 32);
-    canvas.copy(texture, sprite, screen_rect)?;
+    for i in 0..game.positions.len() {
+        let position = &game.positions[i];
+        let entity_type = &game.entity_types[i];
+
+        let screen_position = Point::new(position.x, position.y);
+        let screen_rect = Rect::from_center(screen_position, 32, 32);
+        let sprite = Rect::new(0, 0, 32, 32);
+
+        canvas.copy(texture, sprite, screen_rect)?;
+    }
+
+    // let screen_position = Point::new(width as i32 / 2, height as i32 / 2);
+    // let screen_rect = Rect::from_center(screen_position, 32, 32);
+
+    // let sprite = Rect::new(0, 0, 32, 32);
+    // canvas.copy(texture, sprite, screen_rect)?;
 
     canvas.present();
 
@@ -38,6 +87,8 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/test-tile.png")?;
 
+    let mut game = Game::new();
+
     let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -46,11 +97,16 @@ fn main() -> Result<(), String> {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
+                Event::MouseButtonDown { x, y, mouse_btn, .. } => {
+                    println!("Mouse x: {}, y: {} mouse_btn: {:?}", x, y, mouse_btn);
+
+                    game.add_entity(Position::new(x, y), EntityType::Turret);
+                },
                 _ => {}
             }
         }
 
-        render(&mut canvas, &texture)?;
+        render(&mut canvas, &texture, &game)?;
         
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
